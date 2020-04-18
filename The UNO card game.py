f6e65af7e4a5a8c175340cf96d9f1f06a9963115
AlexMarchese <alex99marchese@gmail.com>
2020-04-18 12:05:50 +0200

    assignment of roles

--- The UNO card game.py
+++ The UNO card game.py
@@ -1,35 +1,39 @@
 import random
 
 
-def create_deck():
-    color = ['green', 'blue', 'red', 'yellow']
-    value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+2', '+4', 'color_change']
-    typ = ['normal', 'wild']
+def create_deck(): # Julia  input: none
+    color = ['green', 'blue', 'red', 'yellow', 'black']
+    value = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, '+2', 'switch', 'skip', '+4', '', 'color_change']
+   
+    return deck # output: [['color','value/number'], [card]]   i.e. [['green', '7'], []]
+    pass [color, value]
 
-    pass
+    
 
-def shuffle_deck():
-    pass
+def shuffle_deck(deck): # Flurin 
+    pass # output shuffled deck
 
-def distribute_cards():
-    pass
+def distribute_cards(shuffled_deck):  # Nicola
+    pass  # output: 2 list comp = [[],[]] and ply = [[],[]] !Warning please remove akso the 14 cards
+            # shuffled_deck also to be returned 
 
-def lay_first_card():
+def lay_first_card(): # Nicola
     pass # Rule: first card cannot be +2, +4 or color change
-
+"""
 def sequence_of_players(): #this function serves to determine the beginner/starter of the game and the order
     pass    # output is: seq_of_ply (a list)
+"""
+
+def display_deck(ply): #this allows players to see what cards they have  # Julia
+    pass #print() output: ['green of 2', 'black of +4']
 
 
-def display_deck(): #this allows players to see what cards they have
-    pass
-
-def play_card():
+def play_card():  # Alex
     pass # rule: +2, +4, color change cannot played as last
          # rule: if last card is played and before player did not say uno \
          # (typed uno before pass) player cannot lay and gets 2 extra cards
 
-def take_card():
+def take_card(): # Flurin
     pass 
 
 def receive_card():
@@ -46,14 +50,14 @@ def input_error():
 
 def play_the_game():
     pass    #this function serves to allow both types of players (normal user and computer) to play
-
+"""
     seq_of_type_ply = [] # this list creates a sequence with all players per type (i.e. ['computer', 'ply'])
     for i in range(len(seq_of_ply)):
         if seq_of_ply[i] == 'computer':
             seq_of_type_ply.append('computer')
         else:
             seq_of_type_ply.append('ply')
-
+"""
     
 
 def display_the_winner():
@@ -68,14 +72,16 @@ def main():
 
     deck = create_deck()
 
-    print('Welcome to the UNO card game. Select the players to play.\nJust put the names of the players.\n \
-        in case you are playing alone you can choose "computer" as second player.\nSo you can play against \
-            the machine.\nThis option is alo available if you are multiple players already and \
-                you wish having the machine as an additional player.\nHAVE FUN!!!')
+    print('Welcome to the UNO card game. Select the players to play.\nJust put the names of the players.\
+\nIn case you are playing alone you can choose "computer" as second player.\nSo you can play against\
+ the machine.\nThis option is alo available if you are multiple players already and\
+ you wish having the machine as an additional player.\nHAVE FUN!!!')
     
+    ply = input('Write you name: ')
+    """
     ply1 = input('player 1: ')
     ply2 = input('player 2: ')
-
+    """
     """
     if ply1 or ply2 not type == str:
         raise input_error
@@ -90,7 +96,7 @@ def main():
     one_more_game = 'yes'
     while one_more_game == 'yes':
 
-        shuffle_deck()
+        shuffled_deck = shuffle_deck()
 
         distribute_cards()
 
@@ -103,11 +109,11 @@ def main():
         display_the_winner()
 
         one_more_game = input('This was the game. Do you want to play again? If so type "yes", otherwise \
-            press any other key to quit.\nYour answer: ')
+press any other key to quit.\nYour answer: ')
 
 
 if __name__ == "__main__": main()
-    pass
+ 
 
 
 
